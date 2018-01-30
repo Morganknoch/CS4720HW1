6c22c0cf318273262598bb9bf72e06695a0e61bf
Morgan Knoch <morgankn11@gmail.com>
2018-01-30 17:44:17 -0600

    Added eigenvector and eigenvalue calculation. Added plotting for points.

--- CS4720HW1/CS4720HW1.py
+++ CS4720HW1/CS4720HW1.py
@@ -4,7 +4,7 @@
 import scipy.io
 import numpy as np
 import math
-
+from matplotlib import pyplot as plt
 # import the data from .mat file
 data = scipy.io.loadmat("data_class4.mat")
 
@@ -84,11 +84,80 @@ def findEigenvectorsAndEigenValues(cov):
     c = (cov[0][0] * cov[1][1]) - (cov[1][0] * cov[0][1])
 
     # produce the eigenvalues
-    x1 = (-b + math.sqrt(b**2 - (4 * 1 * c)))/2
-    x2 = (-b - math.sqrt(b**2 - (4 * 1 * c)))/2
+    y1 = (-b + math.sqrt(b**2 - (4 * 1 * c)))/2
+    y2 = (-b - math.sqrt(b**2 - (4 * 1 * c)))/2
 
-    # solve for eigenvectors
+    # Vectors
+    v1 = []
+    v2 = []
+
+    # solve for first eigenvector
+    if (cov[0][0]-y1) != 0 and cov[0][1] != 0:
+        v1.append((cov[0][1]))
+        v1.append((y1-cov[0][0]))
+    else:
+        v1.append((y1-cov[1][1]))
+        v1.append((cov[1][0]))
+
+    # solve for second eigenvector
+    if (cov[0][0]-y2) != 0 and cov[0][1] != 0:
+        v2.append((cov[0][1]))
+        v2.append((y2-cov[0][0]))
+    else:
+        v2.append((y2-cov[1][1]))
+        v2.append((cov[1][0]))
+
+    # normalize vectors
+    sumv1 = math.sqrt((v1[0]**2 + v1[1]**2))
+    sumv2 = math.sqrt((v2[0]**2 + v2[1]**2))
+
+    v1[0] = v1[0]/sumv1
+    v1[1] = v1[1]/sumv1
+    v2[0] = v2[0]/sumv2
+    v2[1] = v2[1]/sumv2
+
+    return v1, v2
+
+firstClassV1, firstClassV2 = findEigenvectorsAndEigenValues(firstClassCovariance)
+secondClassV1, secondClassV2 = findEigenvectorsAndEigenValues(secondClassCovariance)
+thirdClassV1, thirdClassV2 = findEigenvectorsAndEigenValues(thirdClassCovariance)
+fourthClassV1, fourthClassV2 = findEigenvectorsAndEigenValues(fourthClassCovariance)
+
+# Graph the points and the vectors
+
+# first class is blue
+firstClass = np.array(firstClass)
+firstClass = firstClass.T
+
+for i in firstClass:
+    plt.scatter(i[0],i[1], c='b')
+
+# second class is red
+secondClass = np.array(secondClass)
+secondClass = secondClass.T
+
+for i in secondClass:
+    plt.scatter(i[0],i[1], c='r')
+
+# third class is yellow
+thirdClass = np.array(thirdClass)
+thirdClass = thirdClass.T
+
+for i in thirdClass:
+    plt.scatter(i[0],i[1], c='y')
+
+# fourth class is green
+fourthClass = np.array(fourthClass)
+fourthClass = fourthClass.T
+
+for i in fourthClass:
+    plt.scatter(i[0],i[1], c='g')
+
+# plot eigenvectors
 
 
+
+
+plt.show()
 
 print('Done')
